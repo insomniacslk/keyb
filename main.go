baf3dafc116b9b06a84c5b0af20fcd842d73c72a
Andrea Barberio <insomniac@slackware.it>
2020-05-02 23:43:51 +0100

    Removed graphical stuff
    Signed-off-by: Andrea Barberio <insomniac@slackware.it>

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"image"
 	_ "image/png"
 	"log"
-	"os"
-	"time"
 
-	"github.com/faiface/pixel"
-	"github.com/faiface/pixel/pixelgl"
 	"github.com/karalabe/hid"
-	"golang.org/x/image/colornames"
 )
 
 var (
@@ -54,51 +47,7 @@ func (ks keyStatus) String() string {
 	return ret
 }
 
-func loadPicture(path string) (pixel.Picture, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-	img, _, err := image.Decode(file)
-	if err != nil {
-		return nil, err
-	}
-	return pixel.PictureDataFromImage(img), nil
-}
-
-func runimg(ctx context.Context, imgCh <-chan string) {
-	cfg := pixelgl.WindowConfig{
-		Title:  "Pixel Rocks!",
-		Bounds: pixel.R(0, 0, 250, 250),
-		VSync:  true,
-	}
-	win, err := pixelgl.NewWindow(cfg)
-	if err != nil {
-		panic(err)
-	}
-
-	for {
-		select {
-		case imgName := <-imgCh:
-			log.Printf("Got img %s", imgName)
-			pic, err := loadPicture(imgName)
-			if err != nil {
-				panic(err)
-			}
-			sprite := pixel.NewSprite(pic, pic.Bounds())
-			win.Clear(colornames.White)
-			sprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
-			win.Update()
-			time.Sleep(200 * time.Millisecond)
-			win.SetClosed(true)
-		case <-ctx.Done():
-			break
-		}
-	}
-}
-
-func run(imgCh chan string) error {
+func run() error {
 	devices := hid.Enumerate(vendorID, productID)
 	if len(devices) == 0 {
 		return fmt.Errorf("No device found with vendor:product ID %04x:%04xx", vendorID, productID)
@@ -110,9 +59,6 @@ func run(imgCh chan string) error {
 	}
 	defer dev.Close()
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	go pixelgl.Run(func() { runimg(ctx, imgCh) })
 	var buf keyStatus
 	for {
 		n, err := dev.Read(buf.Slice())
@@ -125,22 +71,15 @@ func run(imgCh chan string) error {
 		}
 		s := buf.String()
 		if s == "" {
-			fmt.Println("<no key is pressed>")
+			fmt.Println("<released>")
 		} else {
 			fmt.Println(s)
-			switch s[len(s)-1] {
-			case '1', '3', '7', '9':
-				imgCh <- "angery.png"
-			default:
-				log.Printf("No image bound for %s", s[len(s)-1])
-			}
 		}
 	}
 }
 
 func main() {
-	imgCh := make(chan string, 1)
-	if err := run(imgCh); err != nil {
+	if err := run(); err != nil {
 		log.Fatal(err)
 	}
 }
